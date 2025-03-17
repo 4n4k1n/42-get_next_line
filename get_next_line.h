a2ce13235dae86a0450816c9136519e5cf5044da
4n4k1n <anakin@pregitzer.de>
2025-03-17 14:11:11 +0100

    header file for function prototypes and BUFFER_SIZE definition

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,25 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 22:54:28 by apregitz          #+#    #+#             */
-/*   Updated: 2025/03/13 01:08:03 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/03/17 13:56:46 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1024
 # endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
 
-int	ft_strchr_index(const char *s, int c)
+ssize_t	ft_strclen(const char *str, ssize_t ch);
+char	*ft_strcjoin(char const *s1, char const *s2q);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strcpy(char *dst, const char *src);
 
 #endif
